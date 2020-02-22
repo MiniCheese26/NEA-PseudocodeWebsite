8a4166c436eaa6d881e57e430ef281bfdf07bdd5
Loc0DeD <minicheese26@tuta.io>
2020-02-22 12:40:48 +0000

    Renamed namespace and started work on home controller

---
+++ NEA-WebApplication/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp;
+using PseudocodeProcessor;
+
+namespace NeaWebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ContentResult Index()
+        {
+            return new ContentResult 
+            {
+                ContentType = "text/html",
+                Content = "<div>Hello World</div>"
+            };
+        }
+
+        [HttpPost]
+        public JsonResult Translate(string code, int languageVersion)
+        {
+            if (!Enum.IsDefined(typeof(LanguageVersion), languageVersion))
+                languageVersion = 0;
+            
+            if (string.IsNullOrWhiteSpace(code))
+                return new JsonResult(new {code = string.Empty, failed = true, messages = new[] {"Submitted code argument was null or empty"}});
+            
+            var translationProcessor = new PseudocodeProcessor.CSharpProcessor.CSharpProcessor(code, (LanguageVersion) languageVersion);
+            IPseudocode pseudocode = translationProcessor.TranslateCode();
+            
+            return new JsonResult(new {code = pseudocode.TranslatedCode, failed = pseudocode.HasFailed, messages = pseudocode.GetMessages});
+        }
+    }
+}
--- NEA-WebApplication/Startup.cs
+++ NEA-WebApplication/Startup.cs
@@ -1,9 +1,8 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace NEA_WebApplication
+namespace NeaWebApplication
 {
     public class Startup
     {
@@ -11,6 +10,7 @@ namespace NEA_WebApplication
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -20,10 +20,10 @@ namespace NEA_WebApplication
             {
                 app.UseDeveloperExceptionPage();
             }
-            app.Run(async context =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+
+            //app.UseStaticFiles();
+            //app.UseDefaultFiles();
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
